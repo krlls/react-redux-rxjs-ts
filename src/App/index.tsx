bf7201cd61cc812bbe301777386d501038451942
k.smirnov <k.smirnov@lendohotel.com>
2020-03-18 21:28:46 +0300

    init

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class AppCmp extends React.Component {
+  render() {
+    return (
+          <div>
+            <h1>My React App! 123</h1>
+          </div>
+    )
+  }
+}
+
+export const App = AppCmp
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,52 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const PATH_SRC = path.resolve(__dirname, 'src')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js"
   },
   module: {
-        rules: [
+    rules: [
+      {
+        test: /\.tsx?$/, loader: 'awesome-typescript-loader',
+        options: {configFileName: 'src/tsconfig.json'}
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+      { test: /\.scss$/, use: [
+          MiniCssExtractPlugin.loader,
           {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }},
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]--[local]--[hash:base64:5]',
+              },
+            },
+          },
           {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
           }
         ]
-    },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+    modules: [].concat(PATH_SRC, 'node_modules'),
+  },
   devServer: {
     contentBase: path.join(__dirname, '/dist'),
     compress: true,
@@ -29,6 +55,10 @@ module.exports = {
     plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html"
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[hash].css',
+      chunkFilename: '[id].css',
+    }),
+]
 };
