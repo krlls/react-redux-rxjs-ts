20a644670dffca2b8e802ef9cbfcdc1a3bfcfaa1
k.smirnov <k.smirnov@lendohotel.com>
2020-04-14 13:39:05 +0300

    init

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,7 +4,7 @@ class AppCmp extends React.Component {
   render() {
     return (
           <div>
-            <h1>My React App! 123</h1>
+            <h1>Hi React App</h1>
           </div>
     )
   }
