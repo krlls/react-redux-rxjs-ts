6444dbc118fba58e384ab862635d5814abd1f074
ksmi <kiriksmi@gmail.com>
2020-04-16 18:49:24 +0300

    Add dockerfile (#9)
    * add tests
    
    * add tests
    
    * add tests
    
    * add tests
    
    Co-authored-by: k.smirnov <k.smirnov@lendohotel.com>

---
+++ src/utils/storeUtils.ts
@@ -0,0 +1,20 @@
+import { applyMiddleware } from 'redux'
+import { createEpicMiddleware } from 'redux-observable'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import { actionSanitizer } from './actionsUtils'
+import { Action } from '../actions'
+import { State } from '../store'
+
+const IS_DEV = process.env.NODE_ENV === 'development'
+export const epicMiddleware = createEpicMiddleware<Action, Action, State>({})
+
+const middlewares = applyMiddleware(...[
+  epicMiddleware,
+])
+
+const composeEnhancers = composeWithDevTools({
+  actionSanitizer,
+})
+
+export const storeMiddlewares = IS_DEV ? composeEnhancers(middlewares) : middlewares
