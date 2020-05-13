099ca84c56d9adf11ece205d913424b021de4317
ksmi <kiriksmi@gmail.com>
2020-05-13 17:49:27 +0300

    add tests (#12)
    Co-authored-by: k.smirnov <k.smirnov@lendohotel.com>

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,67 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const merge = require('webpack-merge');
 
-const PATH_SRC = path.resolve(__dirname, 'src')
+const baseConfig = require('./webpack.main.js');
 
-module.exports = {
+module.exports = merge.smart(baseConfig, {
   mode: 'development',
   devtool: "source-map",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/, loader: 'awesome-typescript-loader',
-        options: {configFileName: 'src/tsconfig.json'}
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      { test: /\.(png|woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=100000' },
-      {test: /\.svg$/, use: ['@svgr/webpack'] },
-      { test: /\.scss$/, use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-              modules: {
-                mode: 'local',
-                localIdentName: '[name]--[local]--[hash:base64:5]',
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          }
-        ]
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
-    modules: [].concat(PATH_SRC, 'node_modules'),
-  },
   devServer: {
-    contentBase: path.join(__dirname, '/dist'),
     compress: true,
     port: 3000
   },
-    plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[hash].css',
-      chunkFilename: '[id].css',
-    }),
-]
-};
+})
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
-const PATH_SRC = path.resolve(__dirname, 'src')
+const baseConfig = require('./webpack.main.js');
 
-module.exports = {
+module.exports = merge.smart(baseConfig, {
   mode: 'production',
-  entry: "./src/index.tsx",
   performance: { hints: false },
   output: {
     path: path.join(__dirname, "/docs"),
@@ -18,48 +16,7 @@ module.exports = {
   optimization: {
     minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
   },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/, loader: 'awesome-typescript-loader',
-        options: {configFileName: 'src/tsconfig.json'}
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {test: /\.svg$/, use: ['@svgr/webpack'] },
-      { test: /\.scss$/, use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-              modules: {
-                mode: 'local',
-                localIdentName: '[name]--[local]--[hash:base64:5]',
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-          }
-        ]
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
-    modules: [].concat(PATH_SRC, 'node_modules'),
-  },
     plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-      chunkFilename: '[id].css',
-    }),
-]
-};
+    ]
+})
