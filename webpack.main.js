dd95106d8e261a7b4f8c228758b983bafdff1b14
k.smirnov <k.smirnov@lendohotel.com>
2020-05-13 17:46:46 +0300

    add tests

---
+++ webpack.main.js
@@ -0,0 +1,62 @@
+//dev
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const PATH_SRC = path.resolve(__dirname, 'src')
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, loader: 'awesome-typescript-loader',
+        options: {configFileName: 'src/tsconfig.json'}
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      { test: /\.(png|woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=100000' },
+      {test: /\.svg$/, use: ['@svgr/webpack'] },
+      { test: /\.scss$/, use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]--[local]--[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          }
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+    modules: [].concat(PATH_SRC, 'node_modules'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/dist'),
+    compress: true,
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: '[id].css',
+    }),
+  ]
+};
